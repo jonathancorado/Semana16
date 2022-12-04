566654d74825dea1df2ae714e55ff737fb282fc7
hp <jonathancorado790@gmail.com>
2022-12-04 09:53:13 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Semana16.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bienvenida(string name, int numTimes = 1)
+        {
+            ViewData["name"] = "Hola" + name;
+            ViewData["numTime"]= numTimes;
+            return View();
+        }
+
+        public string Parameters(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, su numeros de intentos es:{numTimes}");
+        }
+    }
+}
